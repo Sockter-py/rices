18972296e32e7869f6da13bf3428931d805b30be
Sockter-py <99278350+Sockter-py@users.noreply.github.com>
2023-11-10 00:47:40 +0000

    Update install.sh

--- rices/install.sh
+++ rices/install.sh
@@ -2,7 +2,7 @@ cp ./.bashrc /home/$USER/.bashrc
 
 cp ./i3/config /home/$USER/.config/i3
 
-sudo pacman -S terminator
+sudo pacman -S gnome-terminal
 sudo pacman -S polybar
 mkdir /etc/polybar
 sudo cp ./config.ini /etc/polybar
@@ -14,6 +14,7 @@ sudo pacman -S nitrogen
 nitrogen /home/sockterx/.wallpapers/archbtw.png
 sudo pacman -S install firefox
 sudo pacman -S install nano
+sudo pacman -S materia-gtk-theme
 
 git clone https://aur.archlinux.org/yay.git
 sudo chown -R  $USER:users yay
@@ -30,6 +31,8 @@ sudo pacman -S ttf-font-awesome
 sudo pacman -S debtap
 
 curl https://zlibrary-asia.se/soft/zlibrary-setup-latest.deb --output zlib.deb
+curl https://codeload.github.com/PapirusDevelopmentTeam/papirus-icon-theme/tar.gz/refs/tags/20231101 --output pap.tar.gz
+
 
 sudo pacman -S cups
 sudo systemctl enable --now cups
@@ -38,4 +41,5 @@ sudo hp-setup -i
 sudo pacman -S system-config-printer
 #sudo pacman -S gtk3-print-backends
 
+sudo pacman -S calibre
 sudo pacman -S discord
